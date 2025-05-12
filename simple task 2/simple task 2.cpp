bfd23053a39ba933eb17a288554b9164376bfeba
YaroslavKubov <yaroslavkubov@gmail.com>
2025-05-13 02:28:17 +0300

    Добавьте файлы проекта.

---
+++ simple task 2/simple task 2.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    string word;
+
+    std::cout << "Enter the name :" ;
+
+    std::cin >> word;
+
+    std::cout << "Hello " << word << "!" << endl;
+
+    return 0;
+}
